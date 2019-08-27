d2c7c54c24788107beca72635453d91d30156abe
Hogsty <dev@MacBook-de-Damien.local>
2019-08-27 18:53:28 +0200

    recyclerView cliquable

--- app/src/main/java/com/hogsty/myapplication/MainActivity.java
+++ app/src/main/java/com/hogsty/myapplication/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyElementAdapter.OnNoteListener {
+
+    private static final String TAG = "MainActivity";
 
     private RecyclerView recyclerView;
     private List<Element> myElements;
@@ -22,17 +26,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Je récupère la RecyclerView déclarée précédemment dans notre activity_mail.xml via la
+        // méthode findViewById() et je l’affecte à ma variable recyclerView
         recyclerView = (RecyclerView)findViewById(R.id.myRecyclerView);
 
+        // On instancie la liste de jeux
         myElements = new ArrayList<>();
-
         myElements.add(new Element("Element cool", "Cet élement est très cool parce que", R.drawable.keyboard, R.drawable.arrow_right));
         myElements.add(new Element("Element nul", "Cet élement est complètement naze et voilà", R.drawable.laptop, R.drawable.arrow_right));
         myElements.add(new Element("Element standard", "Cet élement est très standard", R.drawable.mouse, R.drawable.arrow_right));
 
-        myElementAdapter = new MyElementAdapter(myElements);
+        myElementAdapter = new MyElementAdapter(myElements, this);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        //Ici on utilise la méthode setAdapter pour lier l'adapter à la liste
         recyclerView.setAdapter(myElementAdapter);
     }
-}
+
+        @Override
+        public void onNoteClick(int position) {
+            //myElements.get(position);
+
+//            Intent intent = new Intent(this, NewActivity.class);
+//            intent.putExtra("some_object", "something_else");
+//            startActivity(intent);
+
+            Log.d(TAG, "onNoteClick: clicked." + position);
+        }
+    }
--- app/src/main/java/com/hogsty/myapplication/MyElementAdapter.java
+++ app/src/main/java/com/hogsty/myapplication/MyElementAdapter.java
@@ -1,12 +1,16 @@
 package com.hogsty.myapplication;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.selection.ItemDetailsLookup;
+import androidx.recyclerview.selection.ItemKeyProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -14,9 +18,11 @@ import java.util.List;
 public class MyElementAdapter extends RecyclerView.Adapter<MyElementAdapter.MyViewHolder> {
 
     List<Element> myElements;
+    private OnNoteListener mOnNoteListener;
 
-    public MyElementAdapter(List<Element> myElements) {
+    public MyElementAdapter(List<Element> myElements, OnNoteListener onNoteListener) {
         this.myElements = myElements;
+        this.mOnNoteListener = onNoteListener;
     }
 
     @NonNull
@@ -24,7 +30,7 @@ public class MyElementAdapter extends RecyclerView.Adapter<MyElementAdapter.MyVi
     public MyElementAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.activity_element, parent, false);
-        return new MyViewHolder(view);
+        return new MyViewHolder(view, mOnNoteListener);
     }
 
     @Override
@@ -42,20 +48,25 @@ public class MyElementAdapter extends RecyclerView.Adapter<MyElementAdapter.MyVi
 
 
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener {
+
+        OnNoteListener onNoteListener;
 
         private TextView mName;
         private TextView mDescription;
         private ImageView mImage;
         private ImageView mArrowRight;
 
-        public MyViewHolder(@NonNull View itemView) {
+        public MyViewHolder(@NonNull View itemView, OnNoteListener onNoteListener) {
             super(itemView);
 
             mName = (TextView)itemView.findViewById(R.id.name);
             mDescription = (TextView)itemView.findViewById(R.id.description);
             mImage = (ImageView)itemView.findViewById(R.id.image);
             mArrowRight = (ImageView)itemView.findViewById(R.id.arrow_right);
+            this.onNoteListener = onNoteListener;
+
+            itemView.setOnClickListener(this);
         }
 
         public void display(Element element){
@@ -64,5 +75,63 @@ public class MyElementAdapter extends RecyclerView.Adapter<MyElementAdapter.MyVi
             mImage.setImageResource(element.getImage());
             mArrowRight.setImageResource(element.getArrow());
         }
+
+        @Override
+        public void onClick(View view) {
+            onNoteListener.onNoteClick(getAdapterPosition());
+        }
     }
+
+    public interface OnNoteListener {
+        void onNoteClick(int position);
+    }
+
 }
+
+
+//    public class MyItemKeyProvider extends ItemKeyProvider {
+//        private final List<Element> elementList;
+//
+//        public MyItemKeyProvider(int scope, List<Element> itemList) {
+//            super(scope);
+//            this.elementList = itemList;
+//        }
+//
+//        @Nullable
+//        @Override
+//        public Object getKey(int position) {
+//            return elementList.get(position);
+//        }
+//
+//        @Override
+//        public int getPosition(@NonNull Object key) {
+//            return elementList.indexOf(key);
+//        }
+//    }
+//
+//
+//    public class MyItemLookup extends ItemDetailsLookup {
+//
+//        private final RecyclerView recyclerView;
+//
+//        public MyItemLookup(RecyclerView recyclerView) {
+//            this.recyclerView = recyclerView;
+//        }
+//
+//        @Nullable
+//        @Override
+//        public ItemDetails getItemDetails(@NonNull MotionEvent e) {
+//            View view = recyclerView.findChildViewUnder(e.getX(), e.getY());
+//            if (view != null) {
+//                RecyclerView.ViewHolder viewHolder = recyclerView.getChildViewHolder(view);
+//                if (viewHolder instanceof MyElementAdapter.MyViewHolder) {
+//                    return ((MyElementAdapter.MyViewHolder) viewHolder).getItemDetails();
+//                }
+//            }
+//
+//            return null;
+//        }
+//    }
+
+
+
